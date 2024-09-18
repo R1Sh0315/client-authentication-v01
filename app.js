e38e85ef6ce701107eefc4b071e6226911e0481a
R1Sh0315 <rishikeshbhalekar6@gmail.com>
2024-09-18 22:22:10 +0530

    vercel

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use('/api/auth', authRoutes)
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port http://localhost:${PORT}`);
+// });
+
+
+module.exports = app;
